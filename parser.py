58a7d450432bfd28be518983396243672c118a77
Alexander Vagner <onemybox@gmail.com>
2022-03-12 19:00:13 +0300

    refactored code

--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
 import csv
 
 import requests
+import json
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-user_agent = UserAgent()
-headers = {
-        'user-agent': user_agent.random
-        }
+ua = UserAgent()
+user_agent = ua.random
+
+headers = {'user-agent': user_agent}
 
 
-def get_links_cards():
+def get_data_json():
     """
     Сбор ссылок на карочки товаров в TXT
     """
-    cards_list_url = []
-
+    data = {}
     for i in range(1, 17, 1):
         url = f'https://www.proudmom.ru/collection/brands?page={i}'
         req = requests.get(url=url, headers=headers)
@@ -29,118 +29,156 @@ def get_links_cards():
 
         for card in cards:
             product_id = card.get('data-product-id')
-            cards_list_url.append(
-                f'https://www.proudmom.ru/products_by_id/{product_id}.json'
-            )
+            urlj = f'https://www.proudmom.ru/products_by_id/{product_id}.json'
 
-    with open('cards_list_url.txt', 'a') as file:
-        for line in cards_list_url:
-            file.write(f'{line}\n')
+            data_json = requests.get(urlj)
+            data[product_id] = data_json.json()
+
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-def get_data_from_cards():
+def get_data_and_sav_csv():
     """
-    Сбор необходимых данных из карточек и сохранение их в CSV
+    Сбор необходимых данных и сохранение их в CSV
     """
-    with open('cards_list_url.txt') as file:
+    with open('data.json') as file:
+        data = json.load(file)
 
-        lines = [line.strip() for line in file.readlines()]
+    data_dict = []
+    count = 1
 
-        data_dict = []
+    for key in data.keys():
+        data_sku = data[key]["products"][0]
+        data_sku_var = data_sku["variants"]
 
-        count = 0
+        product_url = f"https://www.proudmom.ru{data_sku['url']}"
+        article = data_sku_var[0]["sku"] + f"-{count}"
+        name_product = data_sku["title"]
+        price = float(data_sku_var[0]["price"]) * 0.8
 
-        for line in lines:
-            req = requests.get(line, headers)
-            result = req.content
+        size_list = [data_sku_var[i]["title"] + "; " for i in range(
+            len(data_sku_var)
+        )]
+        size = ""
+        for item in size_list:
+            size += item
 
-            soup = BeautifulSoup(result, 'lxml')
-            art_br_prod = soup.find(class_='desc_text_prod').text
-            art_br_prod_tech = art_br_prod.strip().split('\n')
-            article_product = art_br_prod_tech[0].strip().split(': ')
+        img_l = [data_sku["images"][i]["original_url"] + "; " for i in range(
+            len(data_sku["images"])
+        )]
+        photo_url = ""
+        for url in img_l:
+            photo_url += url
 
-            article = article_product[1]
-            name_product = soup.find(
-                class_='content_product_description'
-            ).find('form').find('h1').text
+        try:
+            response = requests.get(url=product_url, headers=headers)
+            content = response.content
+            soup = BeautifulSoup(content, 'lxml')
 
-            price = soup.find(
-                class_='content_product_description'
-            ).find('form').find(
-                'div', class_='block_price_product'
-            ).text.strip()
+            try:
+                div_breadcrumb = soup.find_all(
+                    class_="breadcrumb-link bttn-underline"
+                )
+                albom = div_breadcrumb[1].text.strip()
+            except Exception:
+                albom = "Новинки"
+            try:
+                div_info = soup.find(class_="accordion-item__container")
+                product_info = div_info.text.strip()
+            except Exception as error:
+                print(error)
+                product_info = "Нет данных"
 
-            product_info = []
-            product_info_p = soup.find(
-                class_='body_product_tabs_info'
-            ).find(class_='item_tabs_1').find_all('p')
+        except Exception as error:
+            print(error)
+            continue
 
-            for item in product_info_p:
-                product_info.append(item.text)
-            size = []
-            size_option = soup.find(
-                class_='block_size_product'
-            ).find('select').find_all('option')
+        data_temp = {
+            'Рубрика': 'Для женщин',
+            'Подрубрика': 'Женские блузки, кофты',
+            'Альбом': albom,
+            'Артикул': article,
+            'Наименование': name_product,
+            'Вес': '',
+            'Цена': price,
+            'Цена2': '',
+            'Изображения': photo_url,
+            'Описание': product_info,
+            'Стоимость доставки': '',
+            'Ссылка на товар': product_url,
+            'Seo заголовок': '',
+            'Seo описание': '',
+            'Ключевые слова': '',
+            'Показ': '',
+            'Порядок отображения': '',
+            'Ярлык': '',
+            'Ряд по количеству': '',
+            'Параметр: Размер': size,
+            'Ряд3: Количество': '',
+        }
 
-            for item in size_option:
-                size.append(item.text)
-            photo_url = []
-            photo_url_soup = soup.find(
-                'div', class_='product_description'
-            ).find(
-                'div', class_='galery_product_description'
-            ).find(
-                'div', class_='block_img_galery_product_description'
-            ).find(
-                'div', class_='img_galery_product_description'
-            ).find_all('a')
-            for item in photo_url_soup:
-                photo_url.append(item.get('href'))
-            product_url = line
+        data_dict.append(data_temp)
 
-            data = {
-                'Ссылка на товар': product_url,
-                'Артикул': article,
-                'Наименование': name_product,
-                'Цена': price,
-                'Описание': product_info,
-                'Размер': size,
-                'Фото': photo_url
-            }
-
-            count += 1
-            print(f'{count}: {line} is done!')
-
-            data_dict.append(data)
-
-            with open(
-                'data.csv', 'w', newline='', encoding='utf-8-sig'
-            ) as file:
-                writer = csv.writer(file, delimiter=';')
+        with open(
+            'data_final.csv', 'w', newline='', encoding='utf-8-sig'
+        ) as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow([
+                'Рубрика',
+                'Подрубрика',
+                'Альбом',
+                'Артикул',
+                'Название',
+                'Вес',
+                'Цена',
+                'Цена2',
+                'Изображения',
+                'Описание',
+                'Стоимость доставки',
+                'Ссылка на товар',
+                'Seo заголовок',
+                'Seo описание',
+                'Ключевые слова',
+                'Показ',
+                'Порядок отображения',
+                'Ярлык',
+                'Ряд по количеству',
+                'Параметр: Размер',
+                'Ряд3: Количество'
+            ])
+            for item in data_dict:
                 writer.writerow([
-                    'Ссылка на товар',
-                    'Артикул',
-                    'Наименование',
-                    'Цена',
-                    'Описание',
-                    'Размер',
-                    'Фото'
+                    item['Рубрика'],
+                    item['Подрубрика'],
+                    item['Альбом'],
+                    item['Артикул'],
+                    item['Наименование'],
+                    item['Вес'],
+                    item['Цена'],
+                    item['Цена2'],
+                    item['Изображения'],
+                    item['Описание'],
+                    item['Стоимость доставки'],
+                    item['Ссылка на товар'],
+                    item['Seo заголовок'],
+                    item['Seo описание'],
+                    item['Ключевые слова'],
+                    item['Показ'],
+                    item['Порядок отображения'],
+                    item['Ярлык'],
+                    item['Ряд по количеству'],
+                    item['Параметр: Размер'],
+                    item['Ряд3: Количество']
                 ])
-                for item in data_dict:
-                    writer.writerow([
-                        item['Ссылка на товар'],
-                        item['Артикул'],
-                        item['Наименование'],
-                        item['Цена'],
-                        item['Описание'],
-                        item['Размер'],
-                        item['Фото']
-                    ])
+
+        print(f'{count}: {key} is done!')
+        count += 1
 
 
 def main():
-    get_links_cards()
-    # get_data_from_cards()
+    # get_data_json()
+    get_data_and_sav_csv()
 
 
 if __name__ == '__main__':
